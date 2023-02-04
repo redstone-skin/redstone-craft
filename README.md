2cd0dd0979535cb9b53a7de94a6b866060f72757
Yifan Xiao <1993996310@qq.com>
2023-02-04 22:48:50 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# redstone-craft
+# Redstone Craft - 红石皮肤站官服
+
+## 介绍
+
+这里是红石皮肤站官服的仓库。服务端使用 paper 1.19， 属于纯净生存服务器。请在 windows server 上运行此镜像。
---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set YGGDRASIL_URL=https://mcskin.cn/api/yggdrasil
+set AUTHLIB_AGENT=authlib-injector-1.2.1.jar
+set JAVA_BIN=java
+set JAR_NAME=paper-1.19.3-386.jar
+set JVM_ARGS=-javaagent:%AUTHLIB_AGENT%=%YGGDRASIL_URL% -jar %JAR_NAME%
+set MC_ARGS=nogui
+
+set BOOT_CMD=%JAVA_BIN% %JVM_ARGS% %MC_ARGS%
+
+:RUN_SERVER
+%BOOT_CMD%
+
+goto RUN_SERVER
+pause
